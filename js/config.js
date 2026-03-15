d9e9dfa5e6a93a3c8925f617f50eec6863ba2e56
coder <kirill@khanin.pro>
2026-03-15 10:02:03 +0300

    Поправил ссылку, на тг бот постаивл

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
  * Измените значения здесь, и они обновятся на всём сайте.
  */
 const CONFIG = {
-  telegram: 'https://t.me/examplebot',
-  max: 'https://max.ru/examplebot',
+  telegram: 'https://t.me/Glowapprubot',
+  max: 'https://t.me/Glowapprubot',
   email: 'support@glowapp.ru',
   domain: 'glowapp.ru',
 };
